d9eb15be6b52539b7c48622fd028ff8b59b17970
pushki <105063345+pushki123@users.noreply.github.com>
2023-08-02 22:48:18 +0530

    second commit

--- movie_platform/src/Components/TreadingMovies.tsx
+++ movie_platform/src/Components/TreadingMovies.tsx
@@ -1,46 +1,44 @@
 import "./TreandingMovies.css";
-
 function TreandingMovies() {
   return (
     <>
-      <div className="Row-1">
-        <h1>Treanding Movies</h1>
-        <div className="Card">
-          <div className="MovieName">asda</div>
-          <div className="Rating">wqe</div>
-          <div className="releaseYear" id="">
-            zxc
-          </div>
-        </div>
-        <div className="Card">
-          <div className="MovieName">asd</div>
-          <div className="Rating">qwe</div>
-          <div className="releaseYear" id="">
-            zxc
-          </div>
-        </div>
-        <div className="Card">
-          <div className="MovieName">asd</div>
-          <div className="Rating">qwe</div>
-          <div className="releaseYear" id="">
-            zxc
-          </div>
-        </div>
-        <div className="Card">
-          <div className="MovieName">asd</div>
-          <div className="Rating">qwe</div>
-          <div className="releaseYear" id="">
-            zxc
-          </div>
-        </div>
-        <div className="Card">
-          <div className="MovieName">asd</div>
-          <div className="Rating">qwe</div>
-          <div className="releaseYear" id="">
-            zxc
-          </div>
+      <h1>Treanding Movies</h1>
+      <div className="Card poster1">
+        <div className="MovieName">Happiness For Beginners</div>
+        <div className="Rating">8/10</div>
+        <div className="ReleaseYear" id="">
+          2023
         </div>
       </div>
+      <div className="Card poster2">
+        <div className="MovieName">asd</div>
+        <div className="Rating">qwe</div>
+        <div className="ReleaseYear" id="">
+          zxc
+        </div>
+      </div>
+      <div className="Card poster3">
+        <div className="MovieName">asd</div>
+        <div className="Rating">qwe</div>
+        <div className="ReleaseYear" id="">
+          zxc
+        </div>
+      </div>
+      <div className="Card poster4">
+        <div className="MovieName">asd</div>
+        <div className="Rating">qwe</div>
+        <div className="ReleaseYear" id="">
+          zxc
+        </div>
+      </div>
+      <div className="Card poster5">
+        <div className="MovieName">asd</div>
+        <div className="Rating">qwe</div>
+        <div className="ReleaseYear" id="">
+          zxc
+        </div>
+      </div>
+      <button>Show more</button>
       {/* </div> */}
     </>
   );
