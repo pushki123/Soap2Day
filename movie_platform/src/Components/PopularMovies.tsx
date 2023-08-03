3a2a064c1d6c1523be91d5b96c0180156709ca26
pushki <105063345+pushki123@users.noreply.github.com>
2023-08-03 22:45:43 +0530

    Third Commit

---
+++ movie_platform/src/Components/PopularMovies.tsx
@@ -0,0 +1,91 @@
+import "./PopularMovies.css";
+function PopularMovies() {
+  return (
+    <>
+      <h1>Treanding Movies</h1>
+      <div className="PCard poster1P">
+        <div className="PMovieName">Happiness For Beginners</div>
+        <div className="PRating">8/10</div>
+        <div className="PReleaseYear" id="">
+          2023
+        </div>
+        <button className="PMovieButton">
+          hover me
+          <span className="Pfirst"></span>
+          <span className="Psecond"></span>
+          <span className="Pthird"></span>
+          <span className="Pfourth"></span>
+        </button>
+      </div>
+      <div className="PCard poster2P">
+        <div className="PMovieName">asd</div>
+        <div className="PRating">qwe</div>
+        <div className="PReleaseYear" id="">
+          zxc
+        </div>
+        <button className="PMovieButton">
+          hover me
+          <span className="Pfirst"></span>
+          <span className="Psecond"></span>
+          <span className="Pthird"></span>
+          <span className="Pfourth"></span>
+        </button>
+      </div>
+      <div className="PCard poster3P">
+        <div className="PMovieName">asd</div>
+        <div className="PRating">qwe</div>
+        <div className="PReleaseYear" id="">
+          zxc
+        </div>
+        <button className="PMovieButton">
+          hover me
+          <span className="Pfirst"></span>
+          <span className="Psecond"></span>
+          <span className="Pthird"></span>
+          <span className="Pfourth"></span>
+        </button>
+      </div>
+      <div className="PCard poster4P">
+        <div className="PMovieName">asd</div>
+        <div className="PRating">qwe</div>
+        <div className="PReleaseYear" id="">
+          zxc
+        </div>
+        <button className="PMovieButton">
+          hover me
+          <span className="Pfirst"></span>
+          <span className="Psecond"></span>
+          <span className="Pthird"></span>
+          <span className="Pfourth"></span>
+        </button>
+      </div>
+      <div className="PCard poster5P">
+        <div className="PMovieName">asd</div>
+        <div className="PRating">qwe</div>
+        <div className="PReleaseYear" id="">
+          zxc
+        </div>
+        <button className="PMovieButton">
+          hover me
+          <span className="Pfirst"></span>
+          <span className="Psecond"></span>
+          <span className="Pthird"></span>
+          <span className="Pfourth"></span>
+        </button>
+      </div>
+      <div className="PBtnClass">
+        <button className="PShowMore">
+          Show more
+          <span className="PFirst"></span>
+          <span className="PSecond"></span>
+          <span className="PThird"></span>
+          <span className="PFourth"></span>
+        </button>
+      </div>
+
+      {/* </div> */}
+    </>
+  );
+}
+
+export default PopularMovies;
--- movie_platform/src/Components/TreadingMovies.tsx
+++ movie_platform/src/Components/TreadingMovies.tsx
@@ -3,42 +3,86 @@ function TreandingMovies() {
   return (
     <>
       <h1>Treanding Movies</h1>
-      <div className="Card poster1">
-        <div className="MovieName">Happiness For Beginners</div>
-        <div className="Rating">8/10</div>
-        <div className="ReleaseYear" id="">
+      <div className="TCard poster1T">
+        <div className="TMovieName">Happiness For Beginners</div>
+        <div className="TRating">8/10</div>
+        <div className="TReleaseYear" id="">
           2023
         </div>
+        <button className="TMovieButton">
+          hover me
+          <span className="Tfirst"></span>
+          <span className="Tsecond"></span>
+          <span className="Tthird"></span>
+          <span className="Tfourth"></span>
+        </button>
       </div>
-      <div className="Card poster2">
-        <div className="MovieName">asd</div>
-        <div className="Rating">qwe</div>
-        <div className="ReleaseYear" id="">
+      <div className="TCard poster2T">
+        <div className="TMovieName">asd</div>
+        <div className="TRating">qwe</div>
+        <div className="TReleaseYear" id="">
           zxc
         </div>
+        <button className="TMovieButton">
+          hover me
+          <span className="Tfirst"></span>
+          <span className="Tsecond"></span>
+          <span className="Tthird"></span>
+          <span className="Tfourth"></span>
+        </button>
       </div>
-      <div className="Card poster3">
-        <div className="MovieName">asd</div>
-        <div className="Rating">qwe</div>
-        <div className="ReleaseYear" id="">
+      <div className="TCard poster3T">
+        <div className="TMovieName">asd</div>
+        <div className="TRating">qwe</div>
+        <div className="TReleaseYear" id="">
           zxc
         </div>
+        <button className="TMovieButton">
+          hover me
+          <span className="Tfirst"></span>
+          <span className="Tsecond"></span>
+          <span className="Tthird"></span>
+          <span className="Tfourth"></span>
+        </button>
       </div>
-      <div className="Card poster4">
-        <div className="MovieName">asd</div>
-        <div className="Rating">qwe</div>
-        <div className="ReleaseYear" id="">
+      <div className="TCard poster4T">
+        <div className="TMovieName">asd</div>
+        <div className="TRating">qwe</div>
+        <div className="TReleaseYear" id="">
           zxc
         </div>
+        <button className="TMovieButton">
+          hover me
+          <span className="Tfirst"></span>
+          <span className="Tsecond"></span>
+          <span className="Tthird"></span>
+          <span className="Tfourth"></span>
+        </button>
       </div>
-      <div className="Card poster5">
-        <div className="MovieName">asd</div>
-        <div className="Rating">qwe</div>
-        <div className="ReleaseYear" id="">
+      <div className="TCard poster5T">
+        <div className="TMovieName">asd</div>
+        <div className="TRating">qwe</div>
+        <div className="TReleaseYear" id="">
           zxc
         </div>
+        <button className="TMovieButton">
+          hover me
+          <span className="Tfirst"></span>
+          <span className="Tsecond"></span>
+          <span className="Tthird"></span>
+          <span className="Tfourth"></span>
+        </button>
       </div>
-      <button>Show more</button>
+      <div className="TBtnClass">
+        <button className="TShowMore">
+          Show more
+          <span className="TFirst"></span>
+          <span className="TSecond"></span>
+          <span className="TThird"></span>
+          <span className="TFourth"></span>
+        </button>
+      </div>
+
       {/* </div> */}
     </>
   );
