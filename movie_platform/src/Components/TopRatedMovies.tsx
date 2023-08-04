b505e9ce35189d26f071833b7d90089b6b606768
pushki <105063345+pushki123@users.noreply.github.com>
2023-08-04 22:07:29 +0530

    Third commit

---
+++ movie_platform/src/Components/TopRatedMovies.tsx
@@ -0,0 +1,91 @@
+import "./TopRatedMovies.css";
+function TopRatedMovies() {
+  return (
+    <>
+      <h1>Top Rated Movies</h1>
+      <div className="TRCard poster1T">
+        <div className="TRMovieName">Happiness For Beginners</div>
+        <div className="TRRating">8/10</div>
+        <div className="TRReleaseYear" id="">
+          2023
+        </div>
+        <button className="TRMovieButton">
+          hover me
+          <span className="TRfirst"></span>
+          <span className="TRsecond"></span>
+          <span className="TRthird"></span>
+          <span className="TRfourth"></span>
+        </button>
+      </div>
+      <div className="TRCard poster2T">
+        <div className="TRMovieName">asd</div>
+        <div className="TRRating">qwe</div>
+        <div className="TRReleaseYear" id="">
+          zxc
+        </div>
+        <button className="TRMovieButton">
+          hover me
+          <span className="TRfirst"></span>
+          <span className="TRsecond"></span>
+          <span className="TRthird"></span>
+          <span className="TRfourth"></span>
+        </button>
+      </div>
+      <div className="TRCard poster3T">
+        <div className="TRMovieName">asd</div>
+        <div className="TRRating">qwe</div>
+        <div className="TRReleaseYear" id="">
+          zxc
+        </div>
+        <button className="TRMovieButton">
+          hover me
+          <span className="TRfirst"></span>
+          <span className="TRsecond"></span>
+          <span className="TRthird"></span>
+          <span className="TRfourth"></span>
+        </button>
+      </div>
+      <div className="TRCard poster4T">
+        <div className="TRMovieName">asd</div>
+        <div className="TRRating">qwe</div>
+        <div className="TRReleaseYear" id="">
+          zxc
+        </div>
+        <button className="TRMovieButton">
+          hover me
+          <span className="TRfirst"></span>
+          <span className="TRsecond"></span>
+          <span className="TRthird"></span>
+          <span className="TRfourth"></span>
+        </button>
+      </div>
+      <div className="TRCard poster5T">
+        <div className="TRMovieName">asd</div>
+        <div className="TRRating">qwe</div>
+        <div className="TRReleaseYear" id="">
+          zxc
+        </div>
+        <button className="TRMovieButton">
+          hover me
+          <span className="TRfirst"></span>
+          <span className="TRsecond"></span>
+          <span className="TRthird"></span>
+          <span className="TRfourth"></span>
+        </button>
+      </div>
+      <div className="TRBtnClass">
+        <button className="TRShowMore">
+          Show more
+          <span className="TRFirst"></span>
+          <span className="TRSecond"></span>
+          <span className="TRThird"></span>
+          <span className="TRFourth"></span>
+        </button>
+      </div>
+
+      {/* </div> */}
+    </>
+  );
+}
+
+export default TopRatedMovies;
