2f33b6cb5a4fc2a139656605bc0b4278979547ca
pushki <105063345+pushki123@users.noreply.github.com>
2023-08-11 19:55:43 +0530

    Sevent Commit

--- movie_platform/src/Components/TreadingMovies.tsx
+++ movie_platform/src/Components/TreadingMovies.tsx
@@ -2,88 +2,100 @@ import "./TreandingMovies.css";
 function TreandingMovies() {
   return (
     <>
-      <h1>Treanding Movies</h1>
-      <div className="TCard poster1T">
-        <div className="TMovieName">Happiness For Beginners</div>
-        <div className="TRating">8/10</div>
-        <div className="TReleaseYear" id="">
-          2023
+      <div className="Row1">
+        <h1 className="title">Treanding Movies</h1>
+        <div className="TCard poster1T">
+          <div className="TMovieName">Happiness For Beginners</div>
+          <div className="TRating">8/10</div>
+          <div className="TReleaseYear" id="">
+            2023
+          </div>
+          <a href="">
+            <button className="TMovieButton">
+              hover me
+              <span className="Tfirst"></span>
+              <span className="Tsecond"></span>
+              <span className="Tthird"></span>
+              <span className="Tfourth"></span>
+            </button>
+          </a>
         </div>
-        <button className="TMovieButton">
-          hover me
-          <span className="Tfirst"></span>
-          <span className="Tsecond"></span>
-          <span className="Tthird"></span>
-          <span className="Tfourth"></span>
-        </button>
-      </div>
-      <div className="TCard poster2T">
-        <div className="TMovieName">asd</div>
-        <div className="TRating">qwe</div>
-        <div className="TReleaseYear" id="">
-          zxc
+        <div className="TCard poster2T">
+          <div className="TMovieName">asd</div>
+          <div className="TRating">qwe</div>
+          <div className="TReleaseYear" id="">
+            zxc
+          </div>
+          <a href="">
+            <button className="TMovieButton">
+              hover me
+              <span className="Tfirst"></span>
+              <span className="Tsecond"></span>
+              <span className="Tthird"></span>
+              <span className="Tfourth"></span>
+            </button>
+          </a>
         </div>
-        <button className="TMovieButton">
-          hover me
-          <span className="Tfirst"></span>
-          <span className="Tsecond"></span>
-          <span className="Tthird"></span>
-          <span className="Tfourth"></span>
-        </button>
-      </div>
-      <div className="TCard poster3T">
-        <div className="TMovieName">asd</div>
-        <div className="TRating">qwe</div>
-        <div className="TReleaseYear" id="">
-          zxc
+        <div className="TCard poster3T">
+          <div className="TMovieName">asd</div>
+          <div className="TRating">qwe</div>
+          <div className="TReleaseYear" id="">
+            zxc
+          </div>
+          <a href="">
+            <button className="TMovieButton">
+              hover me
+              <span className="Tfirst"></span>
+              <span className="Tsecond"></span>
+              <span className="Tthird"></span>
+              <span className="Tfourth"></span>
+            </button>
+          </a>
         </div>
-        <button className="TMovieButton">
-          hover me
-          <span className="Tfirst"></span>
-          <span className="Tsecond"></span>
-          <span className="Tthird"></span>
-          <span className="Tfourth"></span>
-        </button>
-      </div>
-      <div className="TCard poster4T">
-        <div className="TMovieName">asd</div>
-        <div className="TRating">qwe</div>
-        <div className="TReleaseYear" id="">
-          zxc
+        <div className="TCard poster4T">
+          <div className="TMovieName">asd</div>
+          <div className="TRating">qwe</div>
+          <div className="TReleaseYear" id="">
+            zxc
+          </div>
+          <a href="">
+            <button className="TMovieButton">
+              hover me
+              <span className="Tfirst"></span>
+              <span className="Tsecond"></span>
+              <span className="Tthird"></span>
+              <span className="Tfourth"></span>
+            </button>
+          </a>
         </div>
-        <button className="TMovieButton">
-          hover me
-          <span className="Tfirst"></span>
-          <span className="Tsecond"></span>
-          <span className="Tthird"></span>
-          <span className="Tfourth"></span>
-        </button>
-      </div>
-      <div className="TCard poster5T">
-        <div className="TMovieName">asd</div>
-        <div className="TRating">qwe</div>
-        <div className="TReleaseYear" id="">
-          zxc
+        <div className="TCard poster5T">
+          <div className="TMovieName">asd</div>
+          <div className="TRating">qwe</div>
+          <div className="TReleaseYear" id="">
+            zxc
+          </div>
+          <a href="">
+            <button className="TMovieButton">
+              hover me
+              <span className="Tfirst"></span>
+              <span className="Tsecond"></span>
+              <span className="Tthird"></span>
+              <span className="Tfourth"></span>
+            </button>
+          </a>
         </div>
-        <button className="TMovieButton">
-          hover me
-          <span className="Tfirst"></span>
-          <span className="Tsecond"></span>
-          <span className="Tthird"></span>
-          <span className="Tfourth"></span>
-        </button>
+        <div className="TBtnClass">
+          <a href="">
+            <button className="TShowMore">
+              Show more
+              <span className="TFirst"></span>
+              <span className="TSecond"></span>
+              <span className="TThird"></span>
+              <span className="TFourth"></span>
+            </button>
+          </a>
+        </div>
       </div>
-      <div className="TBtnClass">
-        <button className="TShowMore">
-          Show more
-          <span className="TFirst"></span>
-          <span className="TSecond"></span>
-          <span className="TThird"></span>
-          <span className="TFourth"></span>
-        </button>
-      </div>
-
-      {/* </div> */}
     </>
   );
 }
