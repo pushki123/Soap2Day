c3ea77ad2fa27ae8cf75980d2ce9ecf70e2389d2
pushki <105063345+pushki123@users.noreply.github.com>
2023-08-13 16:13:28 +0530

    sevent commit

--- movie_platform/src/Components/PopularMovies.tsx
+++ movie_platform/src/Components/PopularMovies.tsx
@@ -2,88 +2,88 @@ import "./PopularMovies.css";
 function PopularMovies() {
   return (
     <>
-      <h1>Treanding Movies</h1>
-      <div className="PCard poster1P">
-        <div className="PMovieName">Happiness For Beginners</div>
-        <div className="PRating">8/10</div>
-        <div className="PReleaseYear" id="">
-          2023
+      <div className="Row3">
+        <h1>Treanding Movies</h1>
+        <div className="PCard poster1P">
+          <div className="PMovieName">Happiness For Beginners</div>
+          <div className="PRating">8/10</div>
+          <div className="PReleaseYear" id="">
+            2023
+          </div>
+          <button className="PMovieButton">
+            hover me
+            <span className="Pfirst"></span>
+            <span className="Psecond"></span>
+            <span className="Pthird"></span>
+            <span className="Pfourth"></span>
+          </button>
         </div>
-        <button className="PMovieButton">
-          hover me
-          <span className="Pfirst"></span>
-          <span className="Psecond"></span>
-          <span className="Pthird"></span>
-          <span className="Pfourth"></span>
-        </button>
-      </div>
-      <div className="PCard poster2P">
-        <div className="PMovieName">asd</div>
-        <div className="PRating">qwe</div>
-        <div className="PReleaseYear" id="">
-          zxc
+        <div className="PCard poster2P">
+          <div className="PMovieName">asd</div>
+          <div className="PRating">qwe</div>
+          <div className="PReleaseYear" id="">
+            zxc
+          </div>
+          <button className="PMovieButton">
+            hover me
+            <span className="Pfirst"></span>
+            <span className="Psecond"></span>
+            <span className="Pthird"></span>
+            <span className="Pfourth"></span>
+          </button>
         </div>
-        <button className="PMovieButton">
-          hover me
-          <span className="Pfirst"></span>
-          <span className="Psecond"></span>
-          <span className="Pthird"></span>
-          <span className="Pfourth"></span>
-        </button>
-      </div>
-      <div className="PCard poster3P">
-        <div className="PMovieName">asd</div>
-        <div className="PRating">qwe</div>
-        <div className="PReleaseYear" id="">
-          zxc
+        <div className="PCard poster3P">
+          <div className="PMovieName">asd</div>
+          <div className="PRating">qwe</div>
+          <div className="PReleaseYear" id="">
+            zxc
+          </div>
+          <button className="PMovieButton">
+            hover me
+            <span className="Pfirst"></span>
+            <span className="Psecond"></span>
+            <span className="Pthird"></span>
+            <span className="Pfourth"></span>
+          </button>
         </div>
-        <button className="PMovieButton">
-          hover me
-          <span className="Pfirst"></span>
-          <span className="Psecond"></span>
-          <span className="Pthird"></span>
-          <span className="Pfourth"></span>
-        </button>
-      </div>
-      <div className="PCard poster4P">
-        <div className="PMovieName">asd</div>
-        <div className="PRating">qwe</div>
-        <div className="PReleaseYear" id="">
-          zxc
+        <div className="PCard poster4P">
+          <div className="PMovieName">asd</div>
+          <div className="PRating">qwe</div>
+          <div className="PReleaseYear" id="">
+            zxc
+          </div>
+          <button className="PMovieButton">
+            hover me
+            <span className="Pfirst"></span>
+            <span className="Psecond"></span>
+            <span className="Pthird"></span>
+            <span className="Pfourth"></span>
+          </button>
         </div>
-        <button className="PMovieButton">
-          hover me
-          <span className="Pfirst"></span>
-          <span className="Psecond"></span>
-          <span className="Pthird"></span>
-          <span className="Pfourth"></span>
-        </button>
-      </div>
-      <div className="PCard poster5P">
-        <div className="PMovieName">asd</div>
-        <div className="PRating">qwe</div>
-        <div className="PReleaseYear" id="">
-          zxc
+        <div className="PCard poster5P">
+          <div className="PMovieName">asd</div>
+          <div className="PRating">qwe</div>
+          <div className="PReleaseYear" id="">
+            zxc
+          </div>
+          <button className="PMovieButton">
+            hover me
+            <span className="Pfirst"></span>
+            <span className="Psecond"></span>
+            <span className="Pthird"></span>
+            <span className="Pfourth"></span>
+          </button>
         </div>
-        <button className="PMovieButton">
-          hover me
-          <span className="Pfirst"></span>
-          <span className="Psecond"></span>
-          <span className="Pthird"></span>
-          <span className="Pfourth"></span>
-        </button>
+        <div className="PBtnClass">
+          <button className="PShowMore">
+            Show more
+            <span className="PFirst"></span>
+            <span className="PSecond"></span>
+            <span className="PThird"></span>
+            <span className="PFourth"></span>
+          </button>
+        </div>
       </div>
-      <div className="PBtnClass">
-        <button className="PShowMore">
-          Show more
-          <span className="PFirst"></span>
-          <span className="PSecond"></span>
-          <span className="PThird"></span>
-          <span className="PFourth"></span>
-        </button>
-      </div>
-
-      {/* </div> */}
     </>
   );
 }
