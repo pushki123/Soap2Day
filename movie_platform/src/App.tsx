1c2a4ed5aceec18eec6bda4609255c8a0b248883
pushki <105063345+pushki123@users.noreply.github.com>
2023-08-13 16:15:07 +0530

    Seventh commit

--- movie_platform/src/App.tsx
+++ movie_platform/src/App.tsx
@@ -1,10 +1,21 @@
-import ListGroup from "./Components/ListGroup";
-import Message from "./Components/ListGroup";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+// import Message from "./Components/Navbar";
+import SliderComponent from "./Components/SliderComponent";
+import TreandingMovies from "./Components/TreadingMovies";
+import PopularMovies from "./Components/PopularMovies";
+import TopRatedMovies from "./Components/TopRatedMovies";
+import MoviePage from "./Components/MoviePage";
+import HomePage from "./Components/HomePage";
+import AboutMoviePage from "./Components/AboutMoviePage";
 function App() {
   return (
-    <div>
-      <ListGroup />
-    </div>
+    <>
+      {/* <Navbar /> */}
+      <div className="Background">
+        <HomePage />
+      </div>
+    </>
   );
 }
 
