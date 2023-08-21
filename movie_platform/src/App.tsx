688108605d72d5a3e39f400d1f6add8aedbe7fd5
pushki <105063345+pushki123@users.noreply.github.com>
2023-08-21 21:01:19 +0530

    ninth commit

--- movie_platform/src/App.tsx
+++ movie_platform/src/App.tsx
@@ -8,13 +8,25 @@ import TopRatedMovies from "./Components/TopRatedMovies";
 import MoviePage from "./Components/MoviePage";
 import HomePage from "./Components/HomePage";
 import AboutMoviePage from "./Components/AboutMoviePage";
+import LatestMovies from "./Components/LatestMovies";
+// import { Router } from "@mui/icons-material";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
       {/* <Navbar /> */}
-      <div className="Background">
-        <HomePage />
-      </div>
+      {
+        /* <LatestMovies /> */
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/AboutMoviePage" element={<AboutMoviePage />}></Route>
+            <Route path="/MoviePage" element={<MoviePage />}></Route>
+          </Routes>
+        </Router>
+      }
     </>
   );
 }
