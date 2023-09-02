e2b5d12480d3fad8fa6fd397c09f5c042ff68e9e
pushki <105063345+pushki123@users.noreply.github.com>
2023-09-02 15:02:43 +0530

    12th commit

---
+++ movie_platform/src/Components/TM3.tsx
@@ -0,0 +1,30 @@
+import "./TM3.css";
+
+function TM3() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <div className="MainBoxTM3">
+        <div className="ContentBoxTM3">
+          <div className="VideoBoxTM3"></div>
+          <div className="MovieNameTM3">Meg 2: The Trench</div>
+          <div className="MovieInfoTM3">
+            An exploratory dive into the deepest depths of the ocean of a daring
+            research team spirals into chaos when a malevolent mining operation
+            threatens their mission and forces them into a high-stakes battle
+            for survival.
+          </div>
+          <div className="RealeaseDateTM3">Release Date: 2023-08-02</div>
+          <div className="TagsTM3">
+            <div className="TagNameTM3">Action</div>
+            <div className="TagNameTM3">Science Fiction</div>
+            <div className="TagNameTM3">Horror</div>
+          </div>
+          <div className="RatingTM3"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TM3;
