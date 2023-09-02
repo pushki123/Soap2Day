b01599f501b6309c86f7c015d9caebe8a60057f4
pushki <105063345+pushki123@users.noreply.github.com>
2023-09-02 15:03:01 +0530

    12th commit

---
+++ movie_platform/src/Components/TM4.tsx
@@ -0,0 +1,30 @@
+import "./TM4.css";
+
+function TM4() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <div className="MainBoxTM4">
+        <div className="ContentBoxTM4">
+          <div className="VideoBoxTM4"></div>
+          <div className="MovieNameTM4">Meg 2: The Trench</div>
+          <div className="MovieInfoTM4">
+            An exploratory dive into the deepest depths of the ocean of a daring
+            research team spirals into chaos when a malevolent mining operation
+            threatens their mission and forces them into a high-stakes battle
+            for survival.
+          </div>
+          <div className="RealeaseDateTM4">Release Date: 2023-08-02</div>
+          <div className="TagsTM4">
+            <div className="TagNameTM4">Action</div>
+            <div className="TagNameTM4">Science Fiction</div>
+            <div className="TagNameTM4">Horror</div>
+          </div>
+          <div className="RatingTM4"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TM4;
