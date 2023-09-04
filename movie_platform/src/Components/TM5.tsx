ecb454d6d6e8d46cd714c40f989931b488dbaf97
pushki <105063345+pushki123@users.noreply.github.com>
2023-09-04 20:41:58 +0530

    14th commit

--- movie_platform/src/Components/TM5.tsx
+++ movie_platform/src/Components/TM5.tsx
@@ -1,30 +1,56 @@
-import "./TM3.css";
+import { useRef, useState } from "react";
+import "./TM5.css";
 
-function TM3() {
+function TM5() {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const handleVideoClick = () => {
+    const video = videoRef.current;
+
+    if (video?.paused) {
+      video.play();
+    } else if (video) {
+      video.pause();
+    }
+
+    setIsVideoPlaying(video?.paused ?? true);
+  };
+
   return (
     <>
       {/* <Navbar /> */}
-      <div className="MainBox">
-        <div className="ContentBox">
-          <div className="VideoBox"></div>
-          <div className="MovieName">Meg 2: The Trench</div>
-          <div className="MovieInfo">
+      <div className="MainBoxTM5">
+        <div className="ContentBoxTM5">
+          <div className="VideoBoxTM5" onClick={handleVideoClick}>
+            {isVideoPlaying ? (
+              <video ref={videoRef} width={"1353px"} height={"700px"} controls>
+                <source src="./TM1Video.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            ) : (
+              // You can display a video thumbnail or play button here
+              <div className="PlayButton"></div>
+            )}
+          </div>
+          <div className="MovieNameTM5">Meg 2: The Trench</div>
+          <div className="MovieInfoTM5">
             An exploratory dive into the deepest depths of the ocean of a daring
             research team spirals into chaos when a malevolent mining operation
             threatens their mission and forces them into a high-stakes battle
             for survival.
           </div>
-          <div className="RealeaseDate">Release Date: 2023-08-02</div>
-          <div className="Tags">
-            <div className="TagName">Action</div>
-            <div className="TagName">Science Fiction</div>
-            <div className="TagName">Horror</div>
+          <div className="RealeaseDateTM5">Release Date: 2023-08-02</div>
+          <div className="TagsTM5">
+            <div className="TagNameTM5">Action</div>
+            <div className="TagNameTM5">Science Fiction</div>
+            <div className="TagNameTM5">Horror</div>
           </div>
-          <div className="Rating"></div>
+          <div className="RatingTM5"></div>
         </div>
       </div>
     </>
   );
 }
 
-export default TM3;
+export default TM5;
