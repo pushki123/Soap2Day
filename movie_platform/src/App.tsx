0e5240cac95266d1eaf0a368e83ce9f1790d9c95
pushki <105063345+pushki123@users.noreply.github.com>
2023-09-13 21:57:11 +0530

    15th commit

--- movie_platform/src/App.tsx
+++ movie_platform/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import Navbar from "./Components/Navbar";
 // import Message from "./Components/Navbar";
 import SliderComponent from "./Components/SliderComponent";
@@ -12,18 +12,40 @@ import LatestMovies from "./Components/LatestMovies";
 // import { Router } from "@mui/icons-material";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PM1 from "./Components/PM1";
+import PM2 from "./Components/PM2";
+import PM3 from "./Components/PM3";
+import PM4 from "./Components/PM4";
+import PM5 from "./Components/PM5";
+import TM1 from "./Components/TM1";
+import TM2 from "./Components/TM2";
+import TM3 from "./Components/TM3";
+import TM4 from "./Components/TM4";
+import TM5 from "./Components/TM5";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
+      {/* <div className="Background">
+        <TM1 />
+      </div> */}
       {
-        /* <LatestMovies /> */
         <Router>
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/AboutMoviePage" element={<AboutMoviePage />}></Route>
             <Route path="/MoviePage" element={<MoviePage />}></Route>
+            <Route path="/LatestMovies" element={<LatestMovies />}></Route>
+            <Route path="/TM1" element={<TM1 />}></Route>
+            <Route path="/TM3" element={<TM3 />}></Route>
+            <Route path="/TM4" element={<TM4 />}></Route>
+            <Route path="/TM5" element={<TM5 />}></Route>
+            <Route path="/TM2" element={<TM2 />}></Route>
+            <Route path="/PM1" element={<PM1 />}></Route>
+            <Route path="/PM3" element={<PM3 />}></Route>
+            <Route path="/PM4" element={<PM4 />}></Route>
+            <Route path="/PM5" element={<PM5 />}></Route>
+            <Route path="/PM2" element={<PM2 />}></Route>
           </Routes>
         </Router>
       }
--- movie_platform/src/Components/TreadingMovies.tsx
+++ movie_platform/src/Components/TreadingMovies.tsx
@@ -1,5 +1,9 @@
 import "./TreandingMovies.css";
 function TreandingMovies() {
+  const handlePlayButtonClick = (path: string) => {
+    window.location.href = path;
+  };
+
   return (
     <>
       <div className="Row1">
@@ -10,15 +14,17 @@ function TreandingMovies() {
           <div className="TReleaseYear" id="">
             2023
           </div>
-          <a href="">
-            <button className="TMovieButton">
-              hover me
-              <span className="Tfirst"></span>
-              <span className="Tsecond"></span>
-              <span className="Tthird"></span>
-              <span className="Tfourth"></span>
-            </button>
-          </a>
+
+          <button
+            className="TMovieButton"
+            onClick={() => handlePlayButtonClick("./TM1")}
+          >
+            Play it
+            <span className="Tfirst"></span>
+            <span className="Tsecond"></span>
+            <span className="Tthird"></span>
+            <span className="Tfourth"></span>
+          </button>
         </div>
         <div className="TCard poster2T">
           <div className="TMovieName">asd</div>
@@ -26,15 +32,17 @@ function TreandingMovies() {
           <div className="TReleaseYear" id="">
             zxc
           </div>
-          <a href="">
-            <button className="TMovieButton">
-              hover me
-              <span className="Tfirst"></span>
-              <span className="Tsecond"></span>
-              <span className="Tthird"></span>
-              <span className="Tfourth"></span>
-            </button>
-          </a>
+
+          <button
+            className="TMovieButton"
+            onClick={() => handlePlayButtonClick("./TM2")}
+          >
+            Play it
+            <span className="Tfirst"></span>
+            <span className="Tsecond"></span>
+            <span className="Tthird"></span>
+            <span className="Tfourth"></span>
+          </button>
         </div>
         <div className="TCard poster3T">
           <div className="TMovieName">asd</div>
@@ -42,15 +50,17 @@ function TreandingMovies() {
           <div className="TReleaseYear" id="">
             zxc
           </div>
-          <a href="">
-            <button className="TMovieButton">
-              hover me
-              <span className="Tfirst"></span>
-              <span className="Tsecond"></span>
-              <span className="Tthird"></span>
-              <span className="Tfourth"></span>
-            </button>
-          </a>
+
+          <button
+            className="TMovieButton"
+            onClick={() => handlePlayButtonClick("./TM1")}
+          >
+            Play it
+            <span className="Tfirst"></span>
+            <span className="Tsecond"></span>
+            <span className="Tthird"></span>
+            <span className="Tfourth"></span>
+          </button>
         </div>
         <div className="TCard poster4T">
           <div className="TMovieName">asd</div>
@@ -58,15 +68,17 @@ function TreandingMovies() {
           <div className="TReleaseYear" id="">
             zxc
           </div>
-          <a href="">
-            <button className="TMovieButton">
-              hover me
-              <span className="Tfirst"></span>
-              <span className="Tsecond"></span>
-              <span className="Tthird"></span>
-              <span className="Tfourth"></span>
-            </button>
-          </a>
+
+          <button
+            className="TMovieButton"
+            onClick={() => handlePlayButtonClick("./TM4")}
+          >
+            Play it
+            <span className="Tfirst"></span>
+            <span className="Tsecond"></span>
+            <span className="Tthird"></span>
+            <span className="Tfourth"></span>
+          </button>
         </div>
         <div className="TCard poster5T">
           <div className="TMovieName">asd</div>
@@ -74,26 +86,26 @@ function TreandingMovies() {
           <div className="TReleaseYear" id="">
             zxc
           </div>
-          <a href="">
-            <button className="TMovieButton">
-              hover me
-              <span className="Tfirst"></span>
-              <span className="Tsecond"></span>
-              <span className="Tthird"></span>
-              <span className="Tfourth"></span>
-            </button>
-          </a>
+
+          <button
+            className="TMovieButton"
+            onClick={() => handlePlayButtonClick("./TM5")}
+          >
+            Play it
+            <span className="Tfirst"></span>
+            <span className="Tsecond"></span>
+            <span className="Tthird"></span>
+            <span className="Tfourth"></span>
+          </button>
         </div>
         <div className="TBtnClass">
-          <a href="">
-            <button className="TShowMore">
-              Show more
-              <span className="TFirst"></span>
-              <span className="TSecond"></span>
-              <span className="TThird"></span>
-              <span className="TFourth"></span>
-            </button>
-          </a>
+          <button className="TShowMore">
+            Show more
+            <span className="TFirst"></span>
+            <span className="TSecond"></span>
+            <span className="TThird"></span>
+            <span className="TFourth"></span>
+          </button>
         </div>
       </div>
     </>
